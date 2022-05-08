031aece592c59f90768ec7216e1e358a87e33251
isaactobiloba <isaactobiloba7@gmail.com>
2022-05-08 12:44:37 +0100

    Create method process and return response

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CampaignCollection;
+use App\Http\Resources\CampaignResource;
 use App\Models\Campaign;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CampaignController extends Controller
 {
@@ -14,7 +17,7 @@ class CampaignController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(new CampaignCollection(Campaign::all()), Response::HTTP_OK);
     }
 
     /**
@@ -25,7 +28,9 @@ class CampaignController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $campaign = Campaign::create($request->only(['name', 'from', 'to', 'total_budget', 'daily_budget']));
+
+        return response()->json(new CampaignResource($campaign), Response::HTTP_CREATED);
     }
 
     /**
@@ -36,7 +41,7 @@ class CampaignController extends Controller
      */
     public function show(Campaign $campaign)
     {
-        //
+        return response()->json(new CampaignResource($campaign), Response::HTTP_OK);
     }
 
     /**
@@ -48,7 +53,9 @@ class CampaignController extends Controller
      */
     public function update(Request $request, Campaign $campaign)
     {
-        //
+        $campaign->update($request->only(['name', 'from', 'to', 'total_budget', 'daily_budget']));
+
+        return response()->json(new CampaignResource($campaign), Response::HTTP_OK);
     }
 
     /**
@@ -59,6 +66,8 @@ class CampaignController extends Controller
      */
     public function destroy(Campaign $campaign)
     {
-        //
+        $campaign->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }

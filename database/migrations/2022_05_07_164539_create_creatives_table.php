47837c025b370bf0e3c91b7925e95ecd746acb6e
isaactobiloba <isaactobiloba7@gmail.com>
2022-05-08 08:14:33 +0100

    Change column name in creatives table

--- database/migrations/2022_05_07_164539_create_creatives_table.php
+++ database/migrations/2022_05_07_164539_create_creatives_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('creatives', function (Blueprint $table) {
             $table->id();
-            $table->string('image_name');
+            $table->string('image_url');
             $table->timestamps();
         });
     }

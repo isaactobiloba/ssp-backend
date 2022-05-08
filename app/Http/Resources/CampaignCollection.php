feee0440d43a932529cac26d9b78e1bf6be97018
isaactobiloba <isaactobiloba7@gmail.com>
2022-05-08 12:45:24 +0100

    return rcampaignCollection response

--- app/Http/Resources/CampaignCollection.php
+++ app/Http/Resources/CampaignCollection.php
@@ -14,6 +14,9 @@ class CampaignCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'applicationName' => 'Supply Side Platform',
+        ];
     }
 }

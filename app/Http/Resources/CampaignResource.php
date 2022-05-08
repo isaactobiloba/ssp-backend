6b26478ed841c833d25e9a4f05ad09a8d518616e
isaactobiloba <isaactobiloba7@gmail.com>
2022-05-08 12:45:43 +0100

    return resource response

--- app/Http/Resources/CampaignResource.php
+++ app/Http/Resources/CampaignResource.php
@@ -14,6 +14,13 @@ class CampaignResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'from' => $this->from,
+            'to' => $this->to,
+            'total_budget' => $this->total_budget,
+            'daily_budget' => $this->daily_budget
+        ];
     }
 }

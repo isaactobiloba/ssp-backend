946d1791515a795c61a6692748da7eb80fb0c26f
isaactobiloba <isaactobiloba7@gmail.com>
2022-05-08 07:48:13 +0100

    Create migration files, add fields/columns

---
+++ database/migrations/2022_05_07_164641_create_campaigns_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('creative_id')->constrained();
+            $table->string('name');
+            $table->date('from');
+            $table->date('to');
+            $table->integer('total_budget');
+            $table->integer('daily_budget');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};

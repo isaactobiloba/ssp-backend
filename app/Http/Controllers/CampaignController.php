5fa29afb40c4cdb4cfaa702dca0d698e32194ef8
isaactobiloba <isaactobiloba7@gmail.com>
2022-06-13 23:37:55 +0100

    Fetch campaigns, change code in store and update methods

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CampaignCollection;
 use App\Http\Resources\CampaignResource;
 use App\Models\Campaign;
+use App\Models\Creative;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -17,7 +18,11 @@ class CampaignController extends Controller
      */
     public function index()
     {
-        return response()->json(new CampaignCollection(Campaign::all()), Response::HTTP_OK);
+        // get campaigns and it's related creatives
+        $campaigns = Campaign::with('creative')->get();
+
+        // return collection of campaigns as a resource
+        return response()->json(new CampaignCollection($campaigns), Response::HTTP_OK);
     }
 
     /**
@@ -28,8 +33,31 @@ class CampaignController extends Controller
      */
     public function store(Request $request)
     {
+        // validate request
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'from' => 'required|date',
+            'to' => 'required|date',
+            'total_budget' => 'required|numeric',
+            'daily_budget' => 'required|numeric',
+        ]);
+
+        // create campaign
         $campaign = Campaign::create($request->only(['name', 'from', 'to', 'total_budget', 'daily_budget']));
 
+        // upload multiple one or multiple images/creatives for each campaign
+        if ($request->hasFile('image_url')) {
+            $files = $request->file('image_url');
+            foreach ($files as $file) {
+                $new_name = rand() . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path('uploads/creatives'), $new_name);
+                // save to database
+                Creative::create([
+                    'image_url' => $new_name,
+                    'campaign_id' => $campaign->id,
+                ]);
+            }
+        }
         return response()->json(new CampaignResource($campaign), Response::HTTP_CREATED);
     }
 
@@ -41,6 +69,9 @@ class CampaignController extends Controller
      */
     public function show(Campaign $campaign)
     {
+        // get campaign and it's related creatives
+        $campaign = Campaign::with('creative')->findOrFail($campaign->id);
+
         return response()->json(new CampaignResource($campaign), Response::HTTP_OK);
     }
 
@@ -53,8 +84,32 @@ class CampaignController extends Controller
      */
     public function update(Request $request, Campaign $campaign)
     {
+        // validate request
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'from' => 'required|date',
+            'to' => 'required|date',
+            'total_budget' => 'required|numeric',
+            'daily_budget' => 'required|numeric',
+        ]);
+
         $campaign->update($request->only(['name', 'from', 'to', 'total_budget', 'daily_budget']));
 
+        // upload multiple one or multiple images/creatives for each campaign
+        if ($request->hasFile('image_url')) {
+            $files = $request->file('image_url');
+            foreach ($files as $file) {
+                $new_name = rand() . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path('uploads/creatives'), $new_name);
+                // save to database
+
+                // check this method, check the update method in laravel
+                Creative::updateOrCreate([
+                    'image_url' => $new_name,
+                    'campaign_id' => $campaign->id,
+                ]);
+            }
+        }
         return response()->json(new CampaignResource($campaign), Response::HTTP_OK);
     }
 

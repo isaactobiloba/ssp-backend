ec93187be3f355874c70447ca4e54ad9aa27c83f
isaactobiloba <isaactobiloba7@gmail.com>
2022-06-13 23:39:48 +0100

    Create controller for creative class

---
+++ app/Http/Controllers/CreativeController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CreativeResource;
+use App\Models\Creative;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CreativeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validate request
+        $request->validate([
+            'image_url' => 'required|string|max:255',
+        ]);
+
+        // upload an array of images
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Creative  $creative
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Creative $creative)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Creative  $creative
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Creative $creative)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Creative  $creative
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Creative $creative)
+    {
+        //
+    }
+}

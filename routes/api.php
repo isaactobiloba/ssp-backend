4725bf7d9fae2c8d12fc7eb7a2dea78e73b2416c
isaactobiloba <isaactobiloba7@gmail.com>
2022-06-13 23:40:25 +0100

    add creatives routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CampaignController;
+use App\Http\Controllers\CreativeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/campaigns', CampaignController::class);
+
+Route::apiResource('/creatives', CreativeController::class);
